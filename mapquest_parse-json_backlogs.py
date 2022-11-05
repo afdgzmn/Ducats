218cac94730315ac991f17a41c8a005046adb8c9
Kean Cabel <72328304+kn-cbl@users.noreply.github.com>
2022-11-05 20:01:31 +0800

    Fixed bugs

--- mapquest_parse-json_backlogs.py
+++ mapquest_parse-json_backlogs.py
@@ -1,13 +1,9 @@
 import urllib.parse
 import requests
-import sys
 from prettytable import PrettyTable
 from colorama import Fore, Style
-from os import system 
 from collections import defaultdict
 from collections import *
-
-system('cls') # clears stdout
 
 def menu():
     print(Fore.RED) # set foreground color to red
@@ -17,31 +13,27 @@ def menu():
     print ("[4] View least visited locations")
     print ("[5] Exit")
 
-menu()
-option = int(input("Enter your option: "))
+def prev(location_lst): 
+    if (len(location_lst) == 0):
+        print("No locations entered yet")
+    else:
+        print(Fore.YELLOW) # set foreground color to yellow
 
-location_lst = [] 
-newloc_lst = [] 
+        #removed the duplicates without having to keep track of the elements’ indices
+        for location in location_lst: 
+            if location not in newloc_lst:
+                newloc_lst.append(location)
+        
+        #display in a numbering format the inputted locations
+        for x, y in enumerate(newloc_lst, 1):
+            print (" ", '{}  {}'.format(x, y))
 
-def prev(): 
-    print(Fore.YELLOW) # set foreground color to yellow
-
-    #removed the duplicates without having to keep track of the elements’ indices
-    for location in location_lst: 
-        if location not in newloc_lst:
-            newloc_lst.append(location)
-    
-    #display in a numbering format the inputted locations
-    for x,y in enumerate(newloc_lst, 1):
-        print (" ", '{}  {}'.format(x,y))
-
-    Total = len(location_lst) #returns length of total inputs from the location_lst
-    print("\nSum of the Total Inputs:", Total) #displays the total inputted locations
+        print("\nTotal Inputs:", len(location_lst)) #displays the total inputted locations
 
 def get_locations(a, setting):
     compare = []
     location_list = []
-    if setting == "min":
+    if setting == 0:
         compare = min(a.items(), key=lambda x: x[1])
     else:
         compare = max(a.items(), key=lambda x: x[1])
@@ -52,75 +44,75 @@ def get_locations(a, setting):
 
     return location_list
 
-def most_visited():
-    print(Fore.GREEN) # set foreground color to green
-
-    temp = defaultdict(int)
-
-    # determine most visited location
-    for sub in location_lst:
-        temp[sub] += 1
-
-    most_visited_places = get_locations(temp, "max")
-
-    # build string
-    places_string = ""
-    for index, item in enumerate(most_visited_places):
-        if index != len(most_visited_places) - 1:
-            places_string += item + ", "
-        else:
-            places_string += item
-
-    if len(most_visited_places) > 1:
-        print("Most Visited Locations are: " + places_string)
+def most_visited(location_lst):
+    if (len(location_lst) == 0):
+        print("No locations entered yet")
     else:
-        print("Most Visited Location is: " + places_string)
-    
-    dup = {}
+        print(Fore.GREEN) # set foreground color to green
 
-    for x in location_lst:
-        if location_lst.count(x) > 0:
-            dup[x] = dup.get(x, 0)+1
+        temp = defaultdict(int)
 
-    print(Style.RESET_ALL)
-    print(Fore.WHITE)
+        # determine most visited location
+        for sub in location_lst:
+            temp[sub] += 1
 
-    print("%s%s" % (dup, ' visit/s'))
+        most_visited_places = get_locations(temp, 0)
 
-def least_visited():
-    print(Fore.CYAN) # set foreground color to cyan
+        # build string
+        places_string = ""
+        for index, item in enumerate(most_visited_places):
+            if index != len(most_visited_places) - 1:
+                places_string += item + ", "
+            else:
+                places_string += item
 
-    temp = defaultdict(int)
-
-    # determine least visited location
-    for sub in location_lst:
-        temp[sub] += 1
-
-    least_visited_places = get_locations(temp, "min")
-
-    # build string
-    places_string = ""
-    for index, item in enumerate(least_visited_places):
-        if index != len(least_visited_places) - 1:
-            places_string += item + ", "
+        if len(most_visited_places) > 1:
+            print("Most Visited Locations are: " + places_string)
         else:
-            places_string += item
+            print("Most Visited Location is: " + places_string)
+        
+        dup = {}
 
-    if len(least_visited_places) > 1:
-        print("Least Visited Locations are: " + places_string)
+        for x in location_lst:
+            if location_lst.count(x) > 0:
+                dup[x] = dup.get(x, 0) + 1
+
+        print("%s%s" % (dup, ' visit/s'))
+
+def least_visited(location_lst):
+    if (len(location_lst) == 0):
+        print("No locations entered yet")
     else:
-        print("Least Visited Location is: " + places_string)
+        print(Fore.CYAN) # set foreground color to cyan
 
-    dup = {}
+        temp = defaultdict(int)
 
-    for x in location_lst:
-        if location_lst.count(x) > 0:
-            dup[x] = dup.get(x, 0) + 1
+        # determine least visited location
+        for sub in location_lst:
+            temp[sub] += 1
 
-    print(Style.RESET_ALL)
-    print(Fore.WHITE)
+        least_visited_places = get_locations(temp, 1)
 
-    print("%s%s" % (dup, ' visit/s'))
+        # build string
+        places_string = ""
+        for index, item in enumerate(least_visited_places):
+            if index != len(least_visited_places) - 1:
+                places_string += item + ", "
+            else:
+                places_string += item
+
+        if len(least_visited_places) > 1:
+            print("Least Visited Locations are: " + places_string)
+        else:
+            print("Least Visited Location is: " + places_string)
+
+        dup = {}
+
+        for x in location_lst:
+            if location_lst.count(x) > 0:
+                dup[x] = dup.get(x, 0) + 1
+
+        print("%s%s" % (dup, ' visit/s'))
 
 def location(location_lst):
     main_api = "https://www.mapquestapi.com/directions/v2/route?"
@@ -129,19 +121,19 @@ def location(location_lst):
     while True:
         print(Fore.YELLOW) # set foreground color to yellow
         orig = input("Starting location: ")
-        location_lst.append(orig)
         if orig.lower() == "quit" or orig.lower() == "q":
-            location_lst.pop(-1) #remove q or quit from array list
             print(Style.RESET_ALL) # remove style
             break
-        
 
         dest = input("Destination: ")
-        location_lst.append(dest)
         if dest.lower() == "quit" or dest.lower() == "q":
-            location_lst.pop(-1) #remove q or quit from array list
             print(Style.RESET_ALL) # remove style
             break
+
+        # destination is location to be visited
+        # append destination only after validating input is not quit
+        # format string to title case
+        location_lst.append(dest.title())
 
         url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
         print("URL: " + (url))
@@ -174,21 +166,18 @@ def location(location_lst):
             directions.align = "l" # left align
             print(Fore.CYAN) # set foreground color to cyan
             print(directions)
-            print(Style.RESET_ALL) # remove style
                 
         elif json_status == 402:
             print(Fore.RED) # set foreground color to red, red denotes error
             print("\n**********************************************")
             print("Status Code: " + str(json_status) + "; Invalid user inputs for one or both locations.")
             print("**********************************************\n")
-            print(Style.RESET_ALL) # remove style
                 
         elif json_status == 611:
             print(Fore.RED) # set foreground color to red, red denotes error
             print("\n**********************************************")
             print("Status Code: " + str(json_status) + "; Missing an entry for one or both locations.")
             print("**********************************************\n")
-            print(Style.RESET_ALL) # remove style
             
         else:
             print(Fore.RED) # set foreground color to red, red denotes error
@@ -196,48 +185,39 @@ def location(location_lst):
             print("For Status Code: " + str(json_status) + "; Refer to:")
             print("https://developer.mapquest.com/documentation/directions-api/status-codes")
             print("**********************************************\n")
+
+        print(Style.RESET_ALL) # remove style
+
+if __name__ == "__main__":
+    location_lst = [] 
+    newloc_lst = [] 
+
+    while True:
+        menu()
+        option = input("Enter your option: ")
+
+        if option == "1":
+            #Enter location option
+            location(location_lst)
+            print(Style.RESET_ALL) # remove style
+            
+        elif option == "2":
+            #View previously inputted locations
+            prev(location_lst)
+            print(Style.RESET_ALL) # remove style
+            
+        elif option == "3":
+            #View most visited locations
+            most_visited(location_lst)
             print(Style.RESET_ALL) # remove style
 
-while option != 0:
-    if option == 1:
-        system('cls') # clears stdout
-        #Enter location option
-        print('\n')
-        print("Option 1: Enter Location")
-        location(location_lst)
-        print(Style.RESET_ALL) # remove style
-        
-    elif option == 2:
-        system('cls') # clears stdout
-        #View previously inputted locations
-        print('\n')
-        print("Option 2: View previously inputted locations")
-        prev()
-        print(Style.RESET_ALL) # remove style
+        elif option == "4":
+            #View least visited locations
+            least_visited(location_lst)
+            print(Style.RESET_ALL) # remove style
 
-        
-    elif option == 3:
-        system('cls') # clears stdout
-        #View most visited locations
-        print('\n')
-        print("Option 3: View most visited locations")
-        most_visited()
-        print(Style.RESET_ALL) # remove style
-
-    elif option == 4:
-        system('cls') # clears stdout
-        #View least visited locations
-        print('\n')
-        print("Option 4: View least visited locations")
-        least_visited()
-        print(Style.RESET_ALL) # remove style
-    else:
-        system('cls') # clears stdout
-        print("Thank you for using the program!")
-        print("\nHave a safe trip!")
-        print(Style.RESET_ALL) # remove style
-        sys.exit()
-
-    print()
-    menu()
-    option = int(input("Enter your option: "))
+        else:
+            print("\n\nThank you for using the program!")
+            print("Have a safe trip!")
+            print(Style.RESET_ALL) # remove style
+            break
